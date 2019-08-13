6f2914192a9b25d496fcdbe654bfdf3286d229d9
AbdelIdir <homeworkoracle@protonmail.com>
2019-08-13 16:48:14 +0200

    doing stretch,ttweaking js code

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -17,7 +17,7 @@
 			xlarge:   [ '1281px',  '1680px' ],
 			large:    [ '981px',   '1280px' ],
 			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
+			small:    [ '490px',   '640px'  ],
 			xsmall:   [ null,      '480px'  ]
 		});
 
